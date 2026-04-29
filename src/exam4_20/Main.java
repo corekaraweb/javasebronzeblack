be8ca0ff458b87c9fce2992f39f0a81ae78659d0
らぐち <corekaraweb@gmail.com>
2026-04-29 20:11:58 +0900

    リポジトリ変更テスト2

--- src/exam4_20/Main.java
+++ src/exam4_20/Main.java
@@ -3,7 +3,7 @@ package exam4_20;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブa
+		// TODO 自動生成されたメソッド・スタブ
 		int j = 0;
 		for (int i = 0;i < 2;i++) {
 			while (j <= 1) {
